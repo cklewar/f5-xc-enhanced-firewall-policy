aea03b006472c51d944b9a4266d1b15974e414ba
cklewar <cklewar@t-online.de>
2023-11-10 12:46:25 +0100

    0.11.23 v5

--- locals.tf
+++ locals.tf
@@ -1,11 +1,11 @@
 locals {
-  f5xc_aws_tgw_name = format("%s-%s-%s", var.project_prefix, var.f5xc_aws_tgw_name, var.project_suffix)
-  custom_tags_bigip       = {
+  f5xc_aws_tgw_name = format("%s-%s-efp-%s", var.project_prefix, var.f5xc_aws_tgw_name, var.project_suffix)
+  custom_tags_bigip = {
     f5xc-tenant  = var.f5xc_tenant
-    f5xc-feature = "aws-tgw-nfv-bigip"
+    f5xc-feature = "aws-tgw-nfv-bigip-efp"
   }
-  custom_tags_pan       = {
+  custom_tags_pan = {
     f5xc-tenant  = var.f5xc_tenant
-    f5xc-feature = "aws-tgw-nfv-pan"
+    f5xc-feature = "aws-tgw-nfv-pan-efp"
   }
 }
