4dcabd8c306104f39b54cd4961bc0f3ce0717cd5
cklewar <cklewar@t-online.de>
2023-11-10 11:38:34 +0100

    Launch 0.11.23

---
+++ versions.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_version = ">= 1.3.0"
+  cloud {
+    organization = "cklewar"
+    hostname     = "app.terraform.io"
+
+    workspaces {
+      name = "f5-xc-enhanced-firewall-policy-module"
+    }
+  }
+  
+  required_providers {
+    volterra = {
+      source = "volterraedge/volterra"
+      version = "= 0.11.23"
+    }
+    local = ">= 2.2.3"
+    null = ">= 3.1.1"
+  }
+}
