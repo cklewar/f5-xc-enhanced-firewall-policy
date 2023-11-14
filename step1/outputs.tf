ba809c92223d9fa8e98338b62428ef46065442cb
cklewar <cklewar@t-online.de>
2023-11-14 12:46:41 +0100

    0.11.23 v9

--- step1/outputs.tf
+++ step1/outputs.tf
@@ -15,29 +15,5 @@ output "efp_name_list" {
 }
 
 output "efp_pan_east_west_simple_name" {
-  value = module.efp_pan_east_west_simple.enhanced-firewall-policy["name"]
-}
-
-output "aws_ec2_instance_nfv_node1_internal_interface_ip" {
-  value = module.nfv.nfv["nodes"]["node1"]["instance"] != null ? module.nfv.nfv["nodes"]["node1"]["instance"]["private_ip"] : null
-}
-
-output "aws_ec2_instance_nfv_node2_internal_interface_ip" {
-  value = module.nfv.nfv["nodes"]["node1"]["instance"] != null ? module.nfv.nfv["nodes"]["node2"]["instance"]["private_ip"] : null
-}
-
-output "aws_ec2_instance_nfv_node1_public_dns_name" {
-  value = module.nfv.nfv["nodes"]["node1"]["instance"] != null ? module.nfv.nfv["nodes"]["node1"]["instance"]["public_dns"] : null
-}
-
-output "aws_ec2_instance_nfv_node2_public_dns_name" {
-  value = module.nfv.nfv["nodes"]["node1"]["instance"] != null ? module.nfv.nfv["nodes"]["node2"]["instance"]["public_dns"] : null
-}
-
-output "aws_ec2_web_instance_private_ip" {
-  value = module.web.aws_ec2_instance["private_ip"]
-}
-
-output "aws_ec2_web_instance_public_ip" {
-  value = module.web.aws_ec2_instance["public_ip"]
+  value = module.efp_pan_east_west_simple_vpc.enhanced_firewall_policy["name"]
 }
