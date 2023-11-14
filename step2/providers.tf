4e306d98f5b98ded80800907bd6431ab4c1fb69e
cklewar <cklewar@t-online.de>
2023-11-14 22:58:08 +0100

    0.11.23 v18

--- step2/providers.tf
+++ step2/providers.tf
@@ -1,17 +1,1 @@
-provider "restapi" {
-  uri     = var.f5xc_api_url
-  debug   = true
-  headers = {
-    Accept                      = "application/json"
-    Content-Type                = "application/json"
-    Authorization               = format("APIToken %s", var.f5xc_api_token)
-    x-volterra-apigw-tenant     = data.tfe_outputs.step1.values.tenant
-    Access-Control-Allow-Origin = "*"
-  }
-  create_method         = "PUT"
-  update_method         = "PUT"
-  destroy_method        = "PUT"
-  create_returns_object = true
-}
-
 provider "tfe" {}
